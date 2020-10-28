19bc083b531f0571c2ac8bae7635d57a5bd52596
baobab744 <baobab744@yandex.ru>
2020-10-28 17:34:59 +1000

    1

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame as pg
+import sys
+
+SIZE = WIDTH, HEIGHT = 640, 480
+speed_x, speed_y = 5, 5
+BLACK = (0, 0, 0)
+FPS = 60
+
+pg.init()
+pg.display.set_caption('Мяч')
+screen = pg.display.set_mode(SIZE)
+clock = pg.time.Clock()
+
+
+ball = pg.image.load('img/ball.png')
+ball_rect = ball.get_rect()
+
+while True:
+    for e in pg.event.get():
+        if e.type == pg.QUIT or \
+                e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE:
+            sys.exit(0)
+
+    ball_rect = ball_rect.move(speed_x, speed_y)
+    if ball_rect.left < 0 or ball_rect.right > WIDTH:
+        speed_x = -speed_x
+    if ball_rect.top < 0 or ball_rect.bottom > HEIGHT:
+        speed_y = -speed_y
+
+    screen.fill(BLACK)
+    screen.blit(ball, ball_rect)
+
+    pg.display.update()
+    clock.tick(FPS)
+    pg.display.set_caption(f'Мяч   FPS: {int(clock.get_fps())}')
